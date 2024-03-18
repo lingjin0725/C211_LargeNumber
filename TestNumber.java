aeb279bb97b116a26c8eb8194c175cc572c7b104
Alex LeClair <acleclair@gmail.com>
2024-03-18 11:22:48 -0400

    Create TestNumber.java

---
+++ TestNumber.java
@@ -0,0 +1,77 @@
+/************************************************************
+ * C211 Fall 2023
+ * Project Phase 2
+ * Authors: Dana Vrajitoru
+ * Class: LargeNumber
+ * A test class for the class LargeNumber.
+ ************************************************************/
+
+package phase2;
+
+import java.util.Scanner;
+
+public class TestNumber {
+
+    // A quick test for the methods so far
+    public static void main(String[] args) {
+        testConstructorInt();
+		testCopy();
+    }
+	
+	// Tests the constructor with an integer
+    public static void testConstructorInt() {
+        LargeNumber n = new LargeNumber(628451);
+        System.out.println("The number is: " + n); // test toString
+        System.out.println("The number has " + n.getSize() + " digits.");
+        if (n.getSign() > 0)
+            System.out.println("The number is positive.");
+        else
+            System.out.println("The number is negative.");
+        System.out.println(n.toInt());
+    }
+	
+	// Tests the copy constructor
+    public static void testCopy() {
+        LargeNumber n1, n2;
+        n1 = new LargeNumber(46274);
+        n2 = new LargeNumber(n1);
+        n2.number.set(1, 3);
+        n2.number.add(5);
+        System.out.println("n1: " + n1 + " n2: " + n2);
+    }
+	
+	/////////////////////////////////////////////////////////
+	// Function stubs to be implemented by the teams
+	
+	// Team 6 
+    public static void testMultiply() {
+	}
+	
+	// Team 5
+    public static void testDivide() {
+	}
+	
+	// Team 7
+    public static void testPercent() {
+	}
+	
+	// Team 2
+    public static void testSubtract() {
+	}
+	
+	// Team 6
+	public static void testString() {
+    }
+	
+	// Team 1
+    public static void testAdd() {
+	}
+	
+    // Team 3
+    public static void testCompare() {
+	}
+	
+	// Team 8
+    public static void testPower() {
+	}
+}
